860c12a74b38accac5fe883394ea72e8d59847e4
Reekdev Ray <ray.reekdev@gmail.com>
2024-04-21 00:54:35 +0530

    test

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,19 @@ dotenv.config({
   path: path.resolve(__dirname, '..', `.env.${process.env.NODE_ENV}`),
 });
 
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: `Working ${Date.now()}`,
+  });
+});
+
 console.log('PORT is: ', process.env.PORT);
+
+app.listen(process.env.PORT, () =>
+  console.log(`server started on port: ${process.env.PORT}`)
+);
